7019a6f70916c1d708d189b142a8d27523af0bed
thesohailjafri <66732877+thesohailjafri@users.noreply.github.com>
2025-02-08 21:46:13 +0530

    update global styles for the entire app in theme configuration

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,7 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
-  // custom color for scrollbar
+  // global styles for the entire app
   styles: {
     global: {
       margin: 0,
